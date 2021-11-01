a038868202d21f94b51f2b0deedd98ba931276a3
storeyme2000@gmail.com <78967603+ms2320@users.noreply.github.com>
2021-11-01 12:06:31 +0000

    updated to include new functions

--- drawingTest.js
+++ drawingTest.js
@@ -1,16 +1,6 @@
 // BEGINNING DRAWING WITH JAVASCRIPT
 // CREATING DRAWING FUNCTIONS
 
-
-// SETTING UP THE CANVAS TO BE THE FULL SIZE OF THE PAGE
-// THIS HAS TO BE DONE IN JAVASCRIPT ELSE THE CANVAS IMAGE WILL
-// END UP APPEARING STRETCHED 
-function setupCanvas(){
-    var canvasContext = document.getElementById("frameCanvas").getContext("2d");
-    canvasContext.canvas.width = window.innerWidth;
-    canvasContext.canvas.height = window.innerHeight;
-}
-
 // ADDING FUNCTION TO AUTOMATICALLY RESIZE THE CANVAS
 function resizeCanvas(){
     var c = document.getElementById("frameCanvas");
@@ -18,31 +8,49 @@ function resizeCanvas(){
     c.width = window.innerWidth;
     c.height = window.innerHeight;
 
-    draw();
+    drawLine(0,0,window.innerWidth,window.innerHeight);
+    clearCanvas();
+    drawLine(window.innerWidth,0,0,window.innerHeight);
 }
 
-// ADDING DRAW FUNCTION
-function draw(){
-    drawLine();
+// DRAW A CIRCLE AT A GIVEN POINT, WITH GIVEN RADIUS
+// OPTION TO DRAW THE CIRCLE FILLED OR NOT
+function drawCircle(xPos, yPos, radius, filled){
+    var c = document.getElementById("frameCanvas");
+    var ctx = c.getContext("2d");
+    
+    if (filled){
+        ctx.fillStyle = "red";
+    }
+
+    ctx.beginPath();
+    ctx.arc(xPos,yPos,radius,0,2*Math.PI);
+    ctx.fill();
 }
 
 // DRAWING A LING ACROSS THE WIDTH OF THE CANVAS
-function drawLine(){
+function drawLine(xPosStart,yPosStart,xPosEnd,yPosEnd){
     var c = document.getElementById("frameCanvas");
     var ctx = c.getContext("2d");
 
-    ctx.moveTo(0,0);
-    ctx.lineTo(c.width+0,c.height);
+    ctx.moveTo(xPosStart,yPosStart);
+    ctx.lineTo(xPosEnd,yPosEnd);
     ctx.stroke();
 }
 
+// FUNCTION TO CLEAR CANVAS
+function clearCanvas(){
+    var c = document.getElementById("frameCanvas");
+    var ctx = c.getContext('2d');
 
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.stroke();
+}
 
 // MAIN FUNCTION CALLED HERE, WAITING FOR WINDOW TO FULLY LOAD TO
 // MITIGATE THE TypeError CAUSED BY HAVING SCRIPTS ABOVE
 // NAMES ELEMENTS OF A WEBPAGE
 window.onload = function(){
     window.addEventListener('resize', resizeCanvas, false); //ADDS EVENT LISTENER FOR THE RESIZING OF THE WINDOW
-    setupCanvas();  // SETTING UP THE CANVAS TO BE THE SIZE OF THE WINDOW
     resizeCanvas();
 }
