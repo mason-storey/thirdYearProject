119ad238ea95483ed262aecb00da060eb1c5cd2d
storeyme2000@gmail.com <78967603+ms2320@users.noreply.github.com>
2022-02-03 11:57:41 +0000

    Adding file dropping

--- fileTest.js
+++ fileTest.js
@@ -1,7 +1,23 @@
 function getFile(event){
     const input = event.target
+    console.log(input);
     if ('files' in input && input.files.length > 0){
         placeFileContent(document.getElementById('contentTarget'),input.files[0]);
+    }
+}
+
+function getFileDrop(event){
+    event.preventDefault();
+
+    console.log("called");
+
+    const input = event.target
+
+    var filesDropped = event.dataTransfer.files;
+    console.log(filesDropped[0]);
+    if (filesDropped.length == 1){
+        console.log("placed");
+        placeFileContent(document.getElementById('contentDropTarget'),filesDropped[0]);
     }
 }
 
@@ -20,6 +36,32 @@ function readFileContent(file){
     })
 }
 
+function dropHandle(event){
+    console.log('File(s) dropped onto window');
+
+    event.preventDefault();
+
+    var files = event.dataTransfer.files;
+    if (files.length !== 1) return;
+
+    var reader = new FileReader();
+
+    var readFile = reader.readAsText(files[0]);
+
+    placeFileContent(document.getElementById('contentDropTarget'),readFile);
+
+}
+
+
+function dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  }
+  
+
 window.onload = function(){
     document.getElementById('inputFile').addEventListener('change',getFile);
+    document.getElementById('dropTarget').addEventListener('drop',getFileDrop);
 }
